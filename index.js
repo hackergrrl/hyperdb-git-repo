6ba41b4fb5f7f67892807b7477ec6415a632b3af
noffle <sww@eight45.net>
2018-06-04 14:09:49 -0700

    hackin

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
+var pull = require('pull-stream')
 var createGitHash = require('pull-hash/ext/git')
+var multicb = require('multicb')
 
 module.exports = function (db) {
+  return {
     refs: pull.empty,
     hasObject: function (hash, cb) { cb(null, false) },
     getObject: function (hash, cb) { cb(null, null) },
@@ -14,18 +17,22 @@ module.exports = function (db) {
       objects(null, function next(end, object) {
         if (end === true) return
         if (end) throw end
+        var pending = 2
+        var buf, hash
         pull(
           object.read,
+          createGitHash(object, done),
           pull.collect(function (err, bufs) {
-            if (err) throw err
-            var buf = Buffer.concat(bufs, object.length)
-            createGitHash(object, function (err, hash) {
-              console.error('Got object', hash, object, buf)
-              // TODO: deduce its hash
-              objects(null, next)
-            })
+            if (err) return done(err)
+            buf = Buffer.concat(bufs, object.length)
+            done()
           })
         )
+        function done (err, theHash) {
+          if (theHash) hash = theHash
+          if (--pending) return
+          console.error('Got object', hash, object, buf.length)
+        }
       })
     }
   }
