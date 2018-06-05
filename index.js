a0c11f46941e75bf73e5f55dd42709e775444b09
noffle <sww@eight45.net>
2018-06-04 19:35:19 -0700

    fix: cb when refs and objs are done

--- index.js
+++ index.js
@@ -2,23 +2,24 @@ var toPull = require('stream-to-pull-stream')
 var pull = require('pull-stream')
 var createGitHash = require('pull-hash/ext/git')
 var multicb = require('multicb')
+var debug = require('debug')('hyperdb-git-repo')
 
 module.exports = function (db) {
   return {
     refs: function () {
-      console.error('want refs')
+      debug('want refs')
       return pull(
         toPull(db.createReadStream('git/refs', {recursive:true})),
         pull.map(function (nodes) {
           var node = nodes[0]
-          console.error('refs', node.key, node.value.toString())
+          debug('refs', node.key, node.value.toString())
           return {
             name: node.key.replace('git/', ''),
             hash: node.value.toString()
           }
         }),
         pull.map(function (ref) {
-          console.error('map', ref)
+          debug('map', ref)
           return ref
         })
       )
@@ -27,13 +28,13 @@ module.exports = function (db) {
       return pull.once({ name: 'HEAD', ref: 'refs/heads/master'})
     },
     hasObject: function (hash, cb) {
-      console.error('has', hash)
+      debug('has', hash)
       db.get('git/objects/' + hash + '/info', function (err, nodes) {
         cb(err, !!nodes.length)
       })
     },
     getObject: function (hash, cb) {
-      console.error('get', hash)
+      debug('get', hash)
       db.get('git/objects/' + hash + '/info', function (err, nodes) {
         if (err) return cb(err)
         if (!nodes.length) return cb({notFound:true})
@@ -45,17 +46,28 @@ module.exports = function (db) {
         })
       })
     },
-    update: function (refs, objects) {
+    update: function (refs, objects, cb) {
+      var refsDone = false
+      var objsDone = false
+
       pull(
         refs,
         pull.drain(function (update) {
           db.put('git/' + update.name, update.new, function () {
-            console.error('update ref', update)
+            debug('update ref', update)
           })
+        }, function () {
+          refsDone = true
+          if (refsDone && objsDone) cb()
         })
       )
+
       objects(null, function next(end, object) {
-        if (end === true) return
+        if (end === true) {
+          objsDone = true
+          if (refsDone && objsDone) cb()
+          return
+        }
         if (end) throw end
         var pending = 2
         var buf, hash
@@ -71,14 +83,14 @@ module.exports = function (db) {
         function done (err, theHash) {
           if (theHash) hash = theHash
           if (--pending) return
-          // console.error('updating object', hash, object, buf.length)
+          // debug('updating object', hash, object, buf.length)
           var info = new Buffer(JSON.stringify({
             type: object.type,
             length: object.length
           }))
           db.put('git/objects/' + hash + '/info', info, function () {
             db.put('git/objects/' + hash + '/data', buf, function () {
-              console.error('wrote', hash, 'to hyperdb')
+              debug('wrote', hash, 'to hyperdb')
               objects(null, next)
             })
           })
